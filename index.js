b2e84bd0e7b68645a976934ef103e216910dd01b
GustafJ <gustaf89@hotmail.se>
2021-03-04 14:54:08 +0100

    adding forms to create rooms and users

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.urlencoded({extended: true}));
 
 const mongoose = require('mongoose');
-const connection = mongoose.connect('mongodb://localhost:27017/wacksolutions', {useNewUrlParser: true});
+const connection = mongoose.connect('mongodb://localhost:27017/wacksolutions');
 const db = mongoose.connection;
 
 app.get('/', (req, res) => {
@@ -16,16 +16,33 @@ app.get('/userform', (req, res) => {
   res.render('userform.ejs');
 });
 
+app.get('/roomform', (req, res) => {
+  res.render('roomform.ejs');
+});
+
 app.post('/signup', (req, res) => {
   const UserModel = require('./models/user');
   const user = new UserModel(req.body);
-  console.log(user);
+
   user.save((error, result) => {
-    console.log(result);
+    if(error) {
+      return handleError(error);
+    }
     res.redirect('/');
   });
 });
 
+app.post('/createroom', (req, res) => {
+  const RoomModel = require('./models/room');
+  const room = new RoomModel(req.body);
+
+  room.save((error, result) => {
+    if(error) {
+      return handleError(error);
+    }
+    res.redirect('/');
+  });
+});
 
 
 db.on('error', (error) => {
