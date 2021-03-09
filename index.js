00ce58be6deadb28893665106992f8a1f5be2f6b
Sebastian Carlson <sebcar98@gmail.com>
2021-03-09 09:57:19 +0100

    started working on encrytion

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI
+const db = require('./config/keys').mongoURI;
+const userRoute = require('./routes/UserRoute');
+const roomRoute = require('./routes/RoomRoute');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -9,16 +11,14 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(error => console.log(error));
 
-
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
-app.use('/user', require('./routes/UserRoute'))
-app.use('/room', require('./routes/RoomRoute'))
+app.use('/user', userRoute);
+app.use('/room', roomRoute);
 
 app.get('/', (req, res) => {
   res.render('index');
 });
-
 
 app.listen(port, () => { console.log(`Server running on port ${port}`) });
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,37 +1,54 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const UserModel = require('../models/user');
 
 router.get('/userform', (req, res) => {
     res.render('userform');
 });
 
 router.post('/signup', (req, res) => {
-    const { first_name, last_name, user_name, email, password } = req.body;
+    const { first_name, last_name, username, email, password } = req.body;
     let errors = [];
 
     // check all fields are filled???
-    if (!first_name || !last_name || !user_name || !email || !password) {
+    if (!first_name || !last_name || !username || !email || !password) {
         errors.push({ msg: 'Enter all fields please' });
     }
 
     // password must be 6 char long
-    if (password.length < 6) {
-        errors.push({ msg: 'Password must be 6 character long' })
+    if (password.length <= 6) {
+        errors.push({ msg: 'Password must be atleast 6 character long' });
     }
 
     // check if errors else register user 
-    if (errors.length > 0) {
-        res.render('userform', { errors, first_name, last_name, user_name, email })
-    } else {
-        const UserModel = require('../models/user');
-        const user = new UserModel(req.body);
+    UserModel.findOne({ email }).exec((error, user) => {
+        if (user) {
+            errors.push({ msg: 'Email already registerd' })
+            res.render('userform', { errors, first_name, last_name, username })
+        } else {
+            const newUser = new UserModel(req.body);
+            bcrypt.genSalt(10, (error, salt) => {
+                bcrypt.hash(newUser.password, salt, (error, hash) => {
+                    if (error) {
+                        throw error;
+                    }
 
-        user.save((error, result) => {
-            if (error) {
-                return handleError(error);
-            }
-            res.redirect('/');
-        });
+                    newUser.password = hash;
+
+                    newUser.save((error, result) => {
+                        if (error) {
+                            return handleError(error);
+                        }
+                        res.redirect('/', { success: "You are now registred" });
+                    });
+                })
+            });
+        }
+    });
+
+    if (errors.length > 0) {
+        res.render('userform', { errors, first_name, last_name, username, email })
     }
 });
 
