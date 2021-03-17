89e707f40dd0ab188d45fb141bfa9baba13178d0
Sebastian Carlson <sebcar98@gmail.com>
2021-03-17 12:03:26 +0100

    render users on createchatform

--- public/scripts/roomform.js
+++ public/scripts/roomform.js
@@ -1,11 +1,43 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     const privateSwitch = document.getElementById('private-switch');
     const privateLabel = document.getElementById('private-label');
+    const userList = document.getElementById('user-list');
+    let isFetched = false;
+    let users;
 
-
-
-    privateSwitch.addEventListener('click', e => {
-        const label = privateSwitch.checked ? "Private Room" : "Public Room";
+    privateSwitch.addEventListener('click', async (e) => {
+        let label = 'Public Room';
+        if (privateSwitch.checked) {
+            if (!isFetched) {
+                const response = await fetch('/api/allusers');
+                users = await response.json();
+                isFetched = true;
+            }
+            label = 'Private Room';
+            if (users) {
+                for (user of users) {
+                    const div = document.createElement('div');
+                    const input = document.createElement('input');
+                    const label = document.createElement('label');
+                    div.classList.add('custom-control', 'custom-checkbox');
+                    input.type = 'checkbox';
+                    input.classList.add('custom-control-input');
+                    input.id = user._id;
+                    label.classList.add('custom-control-label');
+                    label.setAttribute("for", user._id);
+                    label.textContent = user.username;
+                    div.append(input, label);
+                    userList.append(div);
+                }
+            }
+            console.log(users);
+        } else {
+            userList.innerHTML = '';
+        }
         privateLabel.textContent = label;
     });
+
+
+
+
 });
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const UserModel = require('../models/user');
 
 router.get('/allusers', (req, res) => {
-
+	UserModel.find().exec((error, users) => {
+		res.json(users);
+	});
 });
 
 module.exports = router;
